efe1f5e45114c9e056c9f2d504a0e3ca2583e786
carlostalaga <cat@carlostalaga.com.au>
2024-06-28 15:55:37 +0930

    social icons

--- inc/social-icons.php
+++ inc/social-icons.php
@@ -10,11 +10,21 @@ function social_icons_lightmode($lightmode = true) {
     ];
     ?>
 <div id="<?php echo esc_attr($mode_id); ?>" class="w-100 d-flex justify-content-start justify-content-md-end">
+    <!-- 
+     * Loop through the $social_icons array and retrieve the corresponding field value using get_field() function.
+     * If the field value exists, execute the code block inside the if statement.
+     -->
     <?php foreach ($social_icons as $key => $value): ?>
     <?php $field = get_field($key, 'option'); ?>
     <?php if ($field): ?>
     <?php
             // Ensure $field is a string. If it's an array, you might need to adjust this logic.
+            /**
+             * Converts an array to a string, separating the elements with a comma.
+             *
+             * @param mixed $field The value to be converted.
+             * @return string The converted string.
+             */
             $field_value = is_array($field) ? implode(', ', $field) : $field;
             ?>
     <a target="_blank" class="text-light" href="<?php echo esc_url($value['prefix'] . $field_value); ?>">
